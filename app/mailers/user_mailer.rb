1c13e0911a7d8f1aa00cad72b55c20451904e57b
Martin Nguyen <martin@vinova.sg>
2013-01-27 02:08:27 +0700

    test send mail on heroku

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,9 @@ class UserMailer < ActionMailer::Base
   default from: "noreply@herokuapp.com"
   def notice(mark)
     @receiver1 = "toannm3110@gmail.com"
-    @receiver2 = "loandt1991@gmail.com"
+    # @receiver2 = "loandt1991@gmail.com"
     @mark = mark
-    mail(to: [@receiver1,@receiver2], subject: "News from UET")
+    # mail(to: [@receiver1,@receiver2], subject: "News from UET")
+    mail(to: @receiver1, subject: "News from UET")
   end
 end
