0c7ccdfcfc479584fceeab9e571ff71b348403bd
Martin Nguyen <toannm3110@gmail.com>
2013-04-06 01:47:57 +0700

    change migration

--- db/migrate/20130404170007_add_subjects_subcribe_to_user.rb
+++ db/migrate/20130404170007_add_subjects_subcribe_to_user.rb
@@ -1,5 +1,5 @@
 class AddSubjectsSubcribeToUser < ActiveRecord::Migration
   def change
-    add_column :users, :subjects_subcribe, :array
+    add_column :users, :subjects_subcribe, :string
   end
 end
