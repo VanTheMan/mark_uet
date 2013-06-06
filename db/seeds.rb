173b8d56832da5cd3f20f5705923dea2da06d072
Martin Nguyen <toannm3110@gmail.com>
2013-06-06 16:54:30 +0700

    add seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Subject.create!(name: "Khoá luận tốt nghiệp", code: "INT4055")
