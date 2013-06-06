64f670ee806a91f0b344441b0bfffaefbb9530fd
Martin Nguyen <toannm3110@gmail.com>
2013-06-06 16:56:34 +0700

    refine subscribe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,10 @@ class UsersController < ApplicationController
     if @user = User.find_or_create({email: params[:user][:email]})
       subjects = params[:user][:subjects_subcribe].split(',')
       subjects.each do |s|
-        if @subject = Subject.search(s)
+        # if @subject = Subject.search(s)
           @user.subjects << @subject
           @user.save
-        end
+        # end
       end
       redirect_to root_path
     end
